4befd961e40558dd38e82d2ac0f7fbdb4a8a3974
attreid <attreid@gmail.com>
2016-10-03 12:02:26 +0200

    oprava modelu

--- src/Model/AclResources/AclResourcesMapper.php
+++ src/Model/AclResources/AclResourcesMapper.php
@@ -35,7 +35,9 @@ class AclResourcesMapper extends Mapper
 			/* @var $rule Acl */
 			$resources[] = $rule->resource->id;
 		}
-		$this->connection->query('DELETE FROM %table WHERE [id] NOT IN %i[]', $this->getTableName(), $resources);
+		if (!empty($resources)) {
+			$this->connection->query('DELETE FROM %table WHERE [id] NOT IN %i[]', $this->getTableName(), $resources);
+		}
 	}
 
 }
